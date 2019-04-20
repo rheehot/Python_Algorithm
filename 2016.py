56c9ce6a90dd1fdc169e5844063124fd74542938
Han-sol Choi <hansol416@naver.com>
2019-04-20 19:59:28 +0900

    2016.py v 0.0.1

---
+++ 2016.py
@@ -0,0 +1,12 @@
+def solution(a, b):
+    month = [31,28,31,30,31,30,31,31,30,31,30,31]
+    day=['SUN','MON','TUE','WED','THU','FRI','SAT']
+    total_day = 0
+    for i in range(0,a-1):
+        total_day += month[i]
+    total_day += b
+    result = total_day % 7 
+    result = ( result + 5 ) % 7    
+    return day[result]
+
+print(solution(5,24))
