f0c677b676358e08116826c153758e7006b8718f
Han-sol Choi <hansol416@naver.com>
2019-04-27 16:27:09 +0900

    다른 해결 방안 추가

--- 19.04.27 124 나라의 숫자.py
+++ 19.04.27 124 나라의 숫자.py
@@ -24,3 +24,19 @@ def counting(n):
 
 for i in range(0,14):
     print(solution(i))
+
+#################
+# 다른 해결 방법 #
+#################
+def sol(n):
+    num = ['1','2','4']
+    answer = ""
+
+    while n > 0:
+        n -= 1
+        answer = num[n % 3] + answer
+        n //= 3
+
+    return answer
+
+print("놀라운 방법",sol(3))
