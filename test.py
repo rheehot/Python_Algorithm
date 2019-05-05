a8363bdb4cbba3d2a469972b9c178ce8bdb61469
또르 <hansol416@naver.com>
2019-05-05 13:08:05 +0900

    practice class

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 # 이 파일은 깃허브에 정상적으로 올라가는지 확인하기 위해 작성하는 파일입니다
 class test():
-    __init__(a,b):
-        _a = a
-        _b = b
+    # 생성자는 요렇게
+    a = 0
+    b = 0
+    def __init__(self, int_a, int_b):
+        self.a = int_a
+        self.b = int_b
     
-    sum():
-        return a+b
+    def sum(self):
+        return self.a + self.b
 
+a = test(10,20)
+
+print(a.sum())
