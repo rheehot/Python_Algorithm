57149ce284a626f693e65221e7e4034fd739b0d2
또르 <hansol416@naver.com>
2019-05-05 21:51:16 +0900

    update use class

--- class_calc.py
+++ class_calc.py
@@ -1,5 +1,6 @@
 # 이 파일은 깃허브에 정상적으로 올라가는지 확인하기 위해 작성하는 파일입니다
 # 클래스를 이용하여, 계산기 프로그램을 한번 구현해 보자.
+# stack을 이용하여 문자열 형태로 입력받은 수식을 계산할 수 있도록 해보자
 class test():
     # 생성자는 요렇게
     a = 0
@@ -12,18 +13,26 @@ class test():
         if target is '+':
             return self.sum()
         elif target is '-':
-            return ""
+            return self.minus()
         elif target is '/':
-            return ""
+            return self.div()
         elif target is '*':
-            return ""
+            return self.multiple()
         else:
             return "Error Operational Symbol"
 
-
     def sum(self):
         return self.a + self.b
+    def minus(self):
+        return self.a - self.b
+    def div(self):
+        return self.a // self.b
+    def multiple(self):
+        return self.a * self.b    
 
 a = test(10,20)
 
 print(a.calc_f('+'))
+print(a.calc_f('-'))
+print(a.calc_f('/'))
+print(a.calc_f('*'))
