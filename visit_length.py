1df8782b76de9148e3e70be67cb4a9b2f1aaeeec
또르 <hansol416@naver.com>
2019-05-07 22:17:18 +0900

    use tuple

---
+++ visit_length.py
@@ -0,0 +1,18 @@
+def solution(dirs):
+    a = { 'x' : 0, 'y' :0 }
+    answer = 7
+    m_list = list(dirs[::])
+    for i in m_list:
+        if i == 'U':
+            a['y'] += 1
+        elif i == 'D':
+            a['y'] -= 1
+        elif i == 'L':
+            a['x'] -= 1
+        elif i == 'R':
+            a['x'] += 1
+        else:
+            break
+    return answer
+
+solution("ULURRDLLU")
