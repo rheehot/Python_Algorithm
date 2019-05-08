7221fbe47952efcfd5ca609f3eeeb515d8f18954
또르 <hansol416@naver.com>
2019-05-08 23:04:09 +0900

    테스트는 통과

--- 기지국설치.py
+++ 기지국설치.py
@@ -6,8 +6,6 @@ def solution(n, stations, w):
         n_l.append(0)
     for i in stations:
         i -= 1
-        # 최고 범위값보다 항상 1 적게 범위를 설정하기 때문에 임의로 1을 더해줌
-        # 삼항 연산자를 통해서 최소값 최대값 범위가 오게 되면, 최소값 혹은 최대값 범위까지 오게 설정
         for j in range(i-w if i-w > 0 else 0,i+w+1 if i+w < n else n):
             n_l[j] += 1
     check_list = []
