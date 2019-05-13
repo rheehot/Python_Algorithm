3d53a51845aa0f0d45c42dc2a5495cdd670d5976
또르 <hansol416@naver.com>
2019-05-14 02:17:31 +0900

    100% 달성 (Feat. ㅇㅇㅇ님)

--- codility_level4_3.py
+++ codility_level4_3.py
@@ -1,33 +1,43 @@
+def solution(N, A):
+    mylist = [0] * N
+    pre_max_flag = False
+    max_val = 0
+    for i in A:
+        if (i == N+1) and pre_max_flag:
+            continue
+        if i <= N:
+            mylist[i-1] += 1
+            if mylist[i-1] > max_val:
+                max_val = mylist[i-1]
+            pre_max_flag = False
+        else:
+            mylist = [max_val] * N
+            pre_max_flag = True
+    return mylist
+
 # 맥스값이 얼마인지 미리 계산
 # check_index로 가장 큰 값의 위치를 알아내기 위해서 값이 증가했을때 check_list에서 그 위치에 해당하는 값의 인덱스로 변경
 # 계산하는 과정을 줄여보자..
 def solution(N,A):
     check_index = 0
-    max_v = max(A)
     check_list = [0] * N
     a_len = len(A)
-    temp = 0
-    for i in range(a_len):
-        temp = A[i]%N-1
-        if A[i]==max_v:
-            if A[i] == 0:
-                check_list = [check_list[check_index]]*N
-            elif i == 0 :
-                if a_len == 1:
-                    check_list[temp] += 1
-                else:
-                    check_list = [check_list[check_index]] * N        
-            else :
-                check_list = [check_list[check_index]] * N
-        else:
+    flag = False
+    for i in A:
+        temp = i-1
+        if (i == N+1) and flag:
+            continue
+        if i <= N:
             check_list[temp] += 1
-            if check_list[check_index] < check_list[temp]:
+            if check_list[temp] > check_list[check_index]:
                 check_index = temp
+            flag = False
+        else:
+            check_list = [check_list[check_index]] * N
+            flag = True
     return check_list
 
-solution(5,[3,3,3,1,2,0])
-solution(5,[3, 4, 4, 6, 1, 4, 4])
-solution(1,[100000,99999,99999,99999])
+solution(5,[3, 4, 4, 6, 1, 4, 4]) 
 """def solution(N, A):
     max_a = max(A)
     check_list = [0]*N
