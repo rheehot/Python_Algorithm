62bb58d07e7e245b358a243d60c958de2c709787
또르 <hansol416@naver.com>
2019-05-13 16:45:18 +0900

    codility 문제풀이 ( 66% )

--- codility_level4_3.py
+++ codility_level4_3.py
@@ -1,16 +1,19 @@
 def solution(N, A):
-    # N이 배열 크기, A에서 값 확인후 모듈러 
     max_a = max(A)
     check_list = list()
     for i in range(N):
         check_list.append(0)
     for i in A:
         if i == max_a:
-            max_c = max(check_list)
-            for j in range(len(check_list)):
-                check_list[j] = max_c
+            if len(A) == 1:
+                check_list[(i%N-1)] += 1
+            else:
+                max_c = max(check_list)
+                for j in range(len(check_list)):
+                    check_list[j] = max_c
         else:
             check_list[(i%N-1)] += 1
     return check_list
 
 solution(5, [3, 4, 4, 6, 1, 4, 4]) 
+solution(1,[1])
