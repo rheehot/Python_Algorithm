46fa8dffd447cc366544ecbc9717f38069f7376c
또르 <hansol416@naver.com>
2019-05-17 17:20:01 +0900

    87%의 ..한계

--- codility_level5_1.py
+++ codility_level5_1.py
@@ -1,11 +1,22 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
-
-def solution(A, B, K):
-    check_list = []
-    for i in range(A,B):
-        if i % K == 0:
-            check_list.append(i)
-    return len(check_list)
-
-print(solution(6,11,2))
+import math
+def solution(A,B,K):
+    if A==B:
+        if A == 0:
+            return 1
+        elif A%K==0:
+            return 1
+        else:
+            return 0
+    else:
+        if B % K == 0:
+            return math.ceil((B+1-A)/K)
+        else:
+            if A % K == 0:
+                return math.ceil((B+1-A)/K)
+            else:
+                if A < K:
+                    return math.ceil((B+1-A)/K)
+                else:
+                    return math.ceil((B+1-A)/K)-1
+# 아아아아 for 문으로 시도하면 정답은 확실하게 나오는데 속도가 나오질 않고 속도를 따지면 문제가 해결이 안되는 뫼비우스의 띠가 발생한다
+# 아우ㅏㅇ뉠누이룬이ㅏㅜㄹㄴ;ㅁ
