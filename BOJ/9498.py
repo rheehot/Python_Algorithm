1e3c8059f61c0be86b1951587bb0249c324fec19
또르 <hansol416@naver.com>
2019-05-24 17:14:19 +0900

    BOJ 9498

---
+++ BOJ/9498.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if n<= 100 and n>89:
+    print('A')
+elif n<=89 and n>79:
+    print('B')
+elif n<=79 and n>69:
+    print('C')
+elif n<=69 and n>59:
+    print('D')
+else:
+    print('F')
