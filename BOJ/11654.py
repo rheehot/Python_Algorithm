bcf673a65914ea50cdb50edeae0492462c376dcb
또르 <hansol416@naver.com>
2019-05-27 23:00:39 +0900

    BOJ 11654

--- BOJ/11654.py
+++ BOJ/11654.py
@@ -4,3 +4,7 @@ import sys
 # 추가적으로 매개변수로 char형의 변수를 넣을 수 있는데 이는 아마 해당 문자를 제외하는거 같다
 temp = sys.stdin.readline().rstrip()
 print(ord(temp))
+
+# 반대로 돌리는 함수는 chr 이다
+# ord chr 을 기억해서 아스키코드에 대한 문제를 풀수있도록 하자
+# chr(65) = 'A' , ord('A') = 65
