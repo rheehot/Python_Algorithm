0f588ab9af982d6aff0937be44a27db8ffb206c6
또르 <hansol416@naver.com>
2019-06-07 17:33:16 +0900

    codility6

---
+++ codility6_1.py
@@ -0,0 +1,7 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    b = list(set(A))
+    return len(b)
