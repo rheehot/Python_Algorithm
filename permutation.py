c0bf1a82566d53889d79f5950cd05bda84cdfde1
또르 <hansol416@naver.com>
2019-06-08 23:57:26 +0900

    permutation.py 출처표기

--- permutation.py
+++ permutation.py
@@ -1,8 +1,11 @@
+# https://shoark7.github.io/programming/algorithm/Permutations-and-Combinations
+# 원 작자의 소스 출처
 def permutation(arr, r):
     # 1.
     arr = sorted(arr)
     used = [0 for _ in range(len(arr))]
-
+    # https://mingrammer.com/underscore-in-python/
+    # 언더스코어(언더바)의 의미를 누구보다 잘 설명 해주심, 간단하게 설명하자면 그냥 무시하는거임, 0을 4개 호출
     def generate(chosen, used):
         # 2.
         if len(chosen) == r:
