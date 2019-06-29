66f251bc7fb6219870baaf1e5567c3884182a67b
또르 <hansol416@naver.com>
2019-06-29 14:35:49 +0900

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,20 @@
+# c = input()
+# print(c)
+# print(type(c))
+# # c = int(c)
+# a, b = map(int,input().split())
+# print(type(a),type(b))
+# print(a+b)
+
+# import sys
+
+# a = list(map(int,sys.stdin.readline().split()))
+
+
+
+b = list()
+for i in range(3):
+    b.append(int(input()))
+print(b)
+b.pop()
+print(b)
